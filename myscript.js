ae2afaf8d930cdcbc6efcdf29c29ddb2d47254e9
ANANYA_SHAHRIN PROMI <ananyapromi3@gmail.com>
2023-08-09 00:36:22 +0600

    connection

---
+++ myscript.js
@@ -0,0 +1,37 @@
+const oracledb = require('oracledb');
+
+async function runApp() {
+    let connection;
+    try{
+    connection = await oracledb.getConnection ({
+        user          : "project",
+        password      : "12345",
+        connectString : "localhost/orclpdb"
+    });
+    console.log("Succesfully connected to Oracle Database");
+
+    const result = await connection.execute(
+        `SELECT delivaryManID, emailID
+         FROM DELIVARYMAN
+         WHERE delivaryManID = :id`,
+        [1001],  // bind value for :id
+    );
+
+    console.log(result);
+    console.log(result.rows);
+    } catch(err){
+        console.log(err);
+    } finally {
+        if(connection)
+        {
+            try{
+                await connection.close();
+            } catch(err)
+            {
+                console.log(err);
+            }
+        }
+    }
+}
+
+runApp();
