1f7a2244733c0c1e53c4f780999038bca05f2dbc
ANANYA_SHAHRIN PROMI <ananyapromi3@gmail.com>
2023-08-11 09:02:56 +0600

    map

--- Pages/books/index.js
+++ Pages/books/index.js
@@ -6,6 +6,7 @@ export default function BooksInfo({ books }) {
   return (
     <div>
       <h1>List of Books</h1>
+      <h2>{books}</h2>
       {books.map((book) => {
         return (
           <div key={book.ISBN}>
@@ -17,29 +18,34 @@ export default function BooksInfo({ books }) {
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   // let connection;
-  let data;
-  try {
-    //   connection = await oracledb.getConnection({
-    //     user: "project",
-    //     password: "12345",
-    //     connectString: "localhost/orclpdb",
-    //   });
-    const response = await runQuery(`SELECT ISBN, TITLE FROM BOOK`);
-    //   const response = await connection.execute(`SELECT ISBN, TITLE FROM BOOK`);
-    data = await response.json();
-  } catch (err) {
-    console.log(err);
-  } finally {
-    //   if (connection) {
-    //     try {
-    //       await connection.close();
-    //     } catch (err) {
-    //       console.log(err);
-    //     }
-    // }
-  }
+  // try {
+  //   connection = await oracledb.getConnection({
+  //     user: "project",
+  //     password: "12345",
+  //     connectString: "localhost/orclpdb",
+  //   });
+  const response = await runQuery(
+    `SELECT ISBN, TITLE FROM BOOK`
+  );
+  // console.log(response);
+  // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  // console.log(response);
+  //   const response = await connection.execute(`SELECT ISBN, TITLE FROM BOOK`);
+  // const data = await response.json();
+  const data = JSON.stringify(response);
+  // } catch (err) {
+  //   console.log(err);
+  // } finally {
+  //   if (connection) {
+  //     try {
+  //       await connection.close();
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  // }
+  // }
   return {
     props: {
       books: data,
--- dbGenerate/query.js
+++ dbGenerate/query.js
@@ -1,18 +1,19 @@
 // import { response } from "express";
 
 import oracledb from "oracledb";
-// oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
-// async function runQuery(query) {
-//   connection = await oracledb.getConnection({
-//     user: "project",
-//     password: "12345",
-//     connectString: "localhost/orclpdb",
-//   });
-//   const response = await connection.execute(query);
-//   await connection.close();
-//   return response;
-// }
+export default async function runQuery(query) {
+  const connection = await oracledb.getConnection({
+    user: "project",
+    password: "12345",
+    connectString: "localhost/orclpdb",
+  });
+  const response = await connection.execute(query);
+    // console.log(response);
+  await connection.close();
+  return response;
+}
 // export async function getStaticProps(context) {
 //   const { params } = context;
 //   const response = await runQuery(`SELECT ISBN, TITLE FROM BOOK`);
